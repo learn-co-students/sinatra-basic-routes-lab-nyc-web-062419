91244ed04b74d70b197cdfa564de905431333b0f
KConlon13 <KyleConlon13@gmail.com>
2019-07-13 17:35:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Kyle Conlon"
+    end
+
+    get '/hometown' do
+        "My hometown is Valley Stream"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Old Friends by Pinegrove"
+    end
 end
