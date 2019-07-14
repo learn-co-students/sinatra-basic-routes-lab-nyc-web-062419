fdb1bb600a3b26c5a14c559e6b9620a3b156702b
Ben Braunstein <benzbraunstein@gmail.com>
2019-07-14 13:13:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Ben"
+  end
+  get "/hometown" do
+    "My hometown is Great Neck"
+  end
+  get "/favorite-song" do
+    "My favorite song is Matt Nathanson - Giants"
+  end
 end
